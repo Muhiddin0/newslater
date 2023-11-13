27933987a2231eb64a8d0641cd5d6541e905ffe0
muhiddin0 <kabraliev2005@gmail.com>
2023-11-13 10:51:40 +0500

    mini vommit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   css: ["~/assets/styles/main.css"],
   modules: ["nuxt-aos", "nuxt-icon"],
 
